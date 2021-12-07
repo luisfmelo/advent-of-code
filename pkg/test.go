0c59191c4b13534eb7d769a0ea196c04cf480734
Luís Melo <luismelo7@gmail.com>
2021-12-07 14:36:58 +0000

    added day 6 solution

---
+++ pkg/test.go
@@ -0,0 +1,14 @@
+package pkg
+
+func IntArrIsEqual(arr1, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	for i := range arr1 {
+		if arr1[i] != arr2[i] {
+			return false
+		}
+	}
+	return true
+}
